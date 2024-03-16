5efa32938812c5d30beecd5a2d9dbb481c55e0b4
Gabriel <gmendes@edu.univali.br>
2024-03-15 21:09:52 -0300

    commit

--- SiteAdocao/scripts/login.js
+++ SiteAdocao/scripts/login.js
@@ -1,3 +1,10 @@
 function getLogin(){
-    
+    var login = document.getElementById("login").value;
+    var senha = document.getElementById("senha").value;
+    if(login == "admin" && senha == "123"){
+        var url = "./servico.html";
+        window.open(url);
+    } else{
+        alert("Login ou senha incorreto(s)")
+    } 
 }
