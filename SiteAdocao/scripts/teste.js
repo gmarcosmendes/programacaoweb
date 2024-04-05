cc62d23c58a9cdfc7ea63088d34d4531a7f8f76d
Gabriel <gmendes@edu.univali.br>
2024-04-05 20:23:29 -0300

    commit

--- SiteAdocao/scripts/teste.js
+++ SiteAdocao/scripts/teste.js
@@ -1,62 +1,77 @@
-/*
+// let precoGasolina = 5.79;
+// let precoDiesel = 4.59;
+// let precoEtanol = 3.79;
+// let kmPorLitro = 12;
+// let distanciaPercorrida = 1580;
+// let tipoCombustivel = "Etanol";
 
-let precoGasolina = 5.79;
-let precoDiesel = 4.59;
-let precoEtanol = 3.79;
-let kmPorLitro = 12;
-let distanciaPercorrida = 1580;
-let tipoCombustivel = "Etanol";
+// let litrosConsumidos = distanciaPercorrida/kmPorLitro;
 
-let litrosConsumidos = distanciaPercorrida/kmPorLitro;
+// if(tipoCombustivel == "Etanol"){
+//     custoViagem = (litrosConsumidos * precoEtanol).toFixed(2);
+// } else if(tipoCombustivel == "Gasolina"){
+//     custoViagem = (litrosConsumidos * precoGasolina).toFixed(2);
+// } else{
+//     custoViagem = (litrosConsumidos * precoDiesel).toFixed(2)
+// }
 
-if(tipoCombustivel == "Etanol"){
-    custoViagem = (litrosConsumidos * precoEtanol).toFixed(2);
-} else if(tipoCombustivel == "Gasolina"){
-    custoViagem = (litrosConsumidos * precoGasolina).toFixed(2);
-} else{
-    custoViagem = (litrosConsumidos * precoDiesel).toFixed(2)
-}
+// console.log("O custo da viagme é R$" + custoViagem);
 
-console.log("O custo da viagme é R$" + custoViagem);
-*/
 
-/*
 
-let ehSexta = true;
-let ehSabado = false;
-console.log(ehSexta);
-console.log(!ehSabado);
 
-*/
-/*
-let numero = 11;
-if(numero % 2 == "0"){
-    console.log(numero + " é par");
-} else {
-    console.log(numero + " é ímpar");
-}
-*/
+
+// let ehSexta = true;
+// let ehSabado = false;
+// console.log(ehSexta);
+// console.log(!ehSabado);
+
+
+
+
+
+// let numero = 11;
+// if(numero % 2 == "0"){
+//     console.log(numero + " é par");
+// } else {
+//     console.log(numero + " é ímpar");
+// }
+
+
+
+
+
+// function calcAreaQuadrado(lado){
+//     let areaQuadrado = lado * lado;
+//     return areaQuadrado
+// }
+
+// function calcAreaCubo(areaQuadrado){
+//     let areaCubo = 6 * areaQuadrado;
+//     return areaCubo
+// }
+
+// let lado = 10;
+// let areaQuadrado = calcAreaQuadrado(lado);
+// console.log("Área do quadrado é " + areaQuadrado);
+
+// let areaCubo = calcAreaCubo(areaQuadrado);
+// console.log("Área do cubo é " + areaCubo);
+
+
+
 
 
 // function teste(){
 //     console.log("apenas um teste")
 // }
 
-// teste();
+// const teste2 = teste;
+// const teste3 = function (){
+//     console.log("um terceiro exemplo")
+// }
+// teste3();
 
-function calcAreaQuadrado(lado){
-    let areaQuadrado = lado * lado;
-    return areaQuadrado
-}
-
-function calcAreaCubo(areaQuadrado){
-    let areaCubo = 6 * areaQuadrado;
-    return areaCubo
-}
-
-let lado = 10;
-let areaQuadrado = calcAreaQuadrado(lado);
-console.log("Área do quadrado é " + areaQuadrado);
-
-let areaCubo = calcAreaCubo(areaQuadrado);
-console.log("Área do cubo é " + areaCubo);
+// (function (){
+//     console.log("apenas um teste");
+// })();
