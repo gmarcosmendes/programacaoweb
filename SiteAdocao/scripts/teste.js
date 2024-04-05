5de8faca8601c3cb6cb75561d7a6daa72875c4dd
Gabriel <gmendes@edu.univali.br>
2024-04-05 20:07:49 -0300

    commit

---
+++ SiteAdocao/scripts/teste.js
@@ -0,0 +1,62 @@
+/*
+
+let precoGasolina = 5.79;
+let precoDiesel = 4.59;
+let precoEtanol = 3.79;
+let kmPorLitro = 12;
+let distanciaPercorrida = 1580;
+let tipoCombustivel = "Etanol";
+
+let litrosConsumidos = distanciaPercorrida/kmPorLitro;
+
+if(tipoCombustivel == "Etanol"){
+    custoViagem = (litrosConsumidos * precoEtanol).toFixed(2);
+} else if(tipoCombustivel == "Gasolina"){
+    custoViagem = (litrosConsumidos * precoGasolina).toFixed(2);
+} else{
+    custoViagem = (litrosConsumidos * precoDiesel).toFixed(2)
+}
+
+console.log("O custo da viagme é R$" + custoViagem);
+*/
+
+/*
+
+let ehSexta = true;
+let ehSabado = false;
+console.log(ehSexta);
+console.log(!ehSabado);
+
+*/
+/*
+let numero = 11;
+if(numero % 2 == "0"){
+    console.log(numero + " é par");
+} else {
+    console.log(numero + " é ímpar");
+}
+*/
+
+
+// function teste(){
+//     console.log("apenas um teste")
+// }
+
+// teste();
+
+function calcAreaQuadrado(lado){
+    let areaQuadrado = lado * lado;
+    return areaQuadrado
+}
+
+function calcAreaCubo(areaQuadrado){
+    let areaCubo = 6 * areaQuadrado;
+    return areaCubo
+}
+
+let lado = 10;
+let areaQuadrado = calcAreaQuadrado(lado);
+console.log("Área do quadrado é " + areaQuadrado);
+
+let areaCubo = calcAreaCubo(areaQuadrado);
+console.log("Área do cubo é " + areaCubo);
