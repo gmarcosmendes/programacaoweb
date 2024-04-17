f81aa9838cc5d1e9691e51e247f35acbfba69981
Josias Pereira <josias.ngpereira@gmail.com>
2024-04-17 15:49:31 -0300

    commit

---
+++ TrabalhoM1Cinema/script.js
@@ -0,0 +1,32 @@
+const modal = document.querySelector('.modal-container')
+const modal1 = document.querySelector('.modal-container1')
+const modal2 = document.querySelector('.modal-container2')
+const modal3 = document.querySelector('.modal-container3')
+
+function openModal(){
+    modal.classList.add('active')
+}
+function closeModal(){
+    modal.classList.remove('active')
+}
+
+function openModal1(){
+    modal1.classList.add('active')
+}
+function closeModal1(){
+    modal1.classList.remove('active')
+}
+
+function openModal2(){
+    modal2.classList.add('active')
+}
+function closeModal2(){
+    modal2.classList.remove('active')
+}
+
+function openModal3(){
+    modal3.classList.add('active')
+}
+function closeModal3(){
+    modal3.classList.remove('active')
+}
